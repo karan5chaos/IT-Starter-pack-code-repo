8ce08d3d9cc6c6e16f323aee00f7c7819d695f5d
Karan piprani <karanpiprani@gmail.com>
2024-02-28 08:59:59 +0530

    Add project files.

---
+++ IT-Starter-pack/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace IT_Starter_pack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.additionalFeaturesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.microsoftOfficeActivationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mincrosoftWindowsActivationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateRepositoriesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new CSUST.Data.TSmartProgressBar();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.menuStrip1.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.additionalFeaturesToolStripMenuItem,
+            this.settingsToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(402, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // additionalFeaturesToolStripMenuItem
+            // 
+            this.additionalFeaturesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.microsoftOfficeActivationToolStripMenuItem,
+            this.mincrosoftWindowsActivationToolStripMenuItem});
+            this.additionalFeaturesToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("additionalFeaturesToolStripMenuItem.Image")));
+            this.additionalFeaturesToolStripMenuItem.Name = "additionalFeaturesToolStripMenuItem";
+            this.additionalFeaturesToolStripMenuItem.Size = new System.Drawing.Size(125, 20);
+            this.additionalFeaturesToolStripMenuItem.Text = "Additional Features";
+            // 
+            // microsoftOfficeActivationToolStripMenuItem
+            // 
+            this.microsoftOfficeActivationToolStripMenuItem.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.microsoftOfficeActivationToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("microsoftOfficeActivationToolStripMenuItem.Image")));
+            this.microsoftOfficeActivationToolStripMenuItem.Name = "microsoftOfficeActivationToolStripMenuItem";
+            this.microsoftOfficeActivationToolStripMenuItem.Size = new System.Drawing.Size(219, 22);
+            this.microsoftOfficeActivationToolStripMenuItem.Text = "Microsoft Office Activation";
+            this.microsoftOfficeActivationToolStripMenuItem.Click += new System.EventHandler(this.microsoftOfficeActivationToolStripMenuItem_Click);
+            // 
+            // mincrosoftWindowsActivationToolStripMenuItem
+            // 
+            this.mincrosoftWindowsActivationToolStripMenuItem.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mincrosoftWindowsActivationToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("mincrosoftWindowsActivationToolStripMenuItem.Image")));
+            this.mincrosoftWindowsActivationToolStripMenuItem.Name = "mincrosoftWindowsActivationToolStripMenuItem";
+            this.mincrosoftWindowsActivationToolStripMenuItem.Size = new System.Drawing.Size(219, 22);
+            this.mincrosoftWindowsActivationToolStripMenuItem.Text = "Mincrosoft Windows Activation";
+            this.mincrosoftWindowsActivationToolStripMenuItem.Click += new System.EventHandler(this.mincrosoftWindowsActivationToolStripMenuItem_Click);
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.updateRepositoriesToolStripMenuItem});
+            this.settingsToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("settingsToolStripMenuItem.Image")));
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // updateRepositoriesToolStripMenuItem
+            // 
+            this.updateRepositoriesToolStripMenuItem.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updateRepositoriesToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("updateRepositoriesToolStripMenuItem.Image")));
+            this.updateRepositoriesToolStripMenuItem.Name = "updateRepositoriesToolStripMenuItem";
+            this.updateRepositoriesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.updateRepositoriesToolStripMenuItem.Text = "Update Repositories";
+            this.updateRepositoriesToolStripMenuItem.Click += new System.EventHandler(this.updateRepositoriesToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("aboutToolStripMenuItem.Image")));
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 196);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(402, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Font = new System.Drawing.Font("Calibri", 8.25F);
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(10, 17);
+            this.toolStripStatusLabel1.Text = "-";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(5);
+            this.panel1.Size = new System.Drawing.Size(402, 172);
+            this.panel1.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel2);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(5, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(5);
+            this.groupBox1.Size = new System.Drawing.Size(392, 162);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Softwares";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.comboBox1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.progressBar1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(5, 19);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(382, 33);
+            this.panel2.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.comboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(92, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(217, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.Location = new System.Drawing.Point(315, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(62, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Install";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Select software:";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.Transparent;
+            this.progressBar1.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.progressBar1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.progressBar1.Location = new System.Drawing.Point(92, 7);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.ProgressBarFillColor = System.Drawing.Color.LightBlue;
+            this.progressBar1.Size = new System.Drawing.Size(182, 21);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(5, 58);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(5);
+            this.groupBox2.Size = new System.Drawing.Size(382, 99);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "-";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.textBox1.Location = new System.Drawing.Point(5, 25);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(372, 69);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TabStop = false;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 218);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Calibri", 8.25F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "IT Starter Pack";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripMenuItem additionalFeaturesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem updateRepositoriesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem microsoftOfficeActivationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mincrosoftWindowsActivationToolStripMenuItem;
+        private CSUST.Data.TSmartProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ IT-Starter-pack/Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.LinkLabel;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Application = System.Windows.Forms.Application;
+using FileMode = System.IO.FileMode;
+using IT_Starter_pack.Properties;
+using LibGit2Sharp;
+using IT_starter_pack;
+
+namespace IT_Starter_pack
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            progressBar1.Width = comboBox1.Width;
+            progressBar1.Height = comboBox1.Height;
+            progressBar1.Location = comboBox1.Location;
+            progressBar1.Size = comboBox1.Size;
+        }
+
+
+        public class ComboboxItem
+        {
+            public string Text { get; set; }
+            public object Value { get; set; }
+            public object Link { get; set; }
+            public object fname { get; set; }
+
+            public override string ToString()
+            {
+                return Text;
+            }
+        }
+
+        void showprogressbar(bool activate)
+        {
+            if (activate)
+            {
+                comboBox1.Visible = false;
+                label3.Text = "Downloading";
+            }
+            else
+            {
+                comboBox1.Visible = true;
+                label3.Text = "Select Software:";
+            }
+            progressBar1.Visible = activate;
+            progressBar1.Value = 0;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            groupBox2.Text = (comboBox1.SelectedItem as ComboboxItem).Text.ToString();
+            textBox1.Text = (comboBox1.SelectedItem as ComboboxItem).Value.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            //backgroundWorker1.RunWorkerAsync();
+            update_repos();
+        }
+
+
+        public void DeleteReadOnlyDirectory(string directory)
+        {
+            try
+            {
+                dpath = Application.StartupPath + "/apps";
+                foreach (var subdirectory in Directory.EnumerateDirectories(directory))
+                {
+                    DeleteReadOnlyDirectory(subdirectory);
+                }
+                foreach (var fileName in Directory.EnumerateFiles(directory))
+                {
+                    var fileInfo = new FileInfo(fileName);
+                    fileInfo.Attributes = FileAttributes.Normal;
+                    fileInfo.Delete();
+                }
+                Directory.Delete(directory);
+            }
+            catch { }
+        }
+
+        void update_repos()
+        {
+            try
+            {
+                if (Directory.Exists(dpath))
+                {
+                    DeleteReadOnlyDirectory(dpath);
+                    //Directory.Delete(dpath, true);
+                }
+
+                Repository.Clone("https://github.com/karan5chaos/IT_starter_pack", dpath);
+
+                var repo = File.ReadAllLines(dpath + "/repo.txt");
+
+
+                foreach (string line in repo)
+                {
+                    string application = line;
+                    string[] application_info = application.Split(';');
+
+                    string appname = application.Split(';')[0];
+                    string description = application.Split(';')[1];
+                    string link = application.Split(';')[2];
+                    string fname = application.Split(';')[3];
+
+                    addtocombobox(appname, description, link, fname);
+
+                }
+            }
+            catch { }   
+        }
+
+        private void Wc_DownloadProgressChanged1(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        ComboboxItem item;
+        void addtocombobox(string text, string value, string link, string fname)
+        {
+            item = new ComboboxItem();
+            item.Text = text;
+            item.Value = value;
+            item.Link = link;
+            item.fname = fname;
+
+            comboBox1.Items.Add(item);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Download_installAsync((comboBox1.SelectedItem as ComboboxItem).Link.ToString(), (comboBox1.SelectedItem as ComboboxItem).fname.ToString());
+            showprogressbar(true);
+        }
+
+        void wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        string dpath = Application.StartupPath+"/apps";
+
+        void Download_installAsync(string link, string fname)
+        {
+            try
+            {
+                dpath = Application.StartupPath + "/apps";
+                button1.Enabled = false;
+
+                using (WebClient wc = new WebClient())
+                {
+                    wc.DownloadProgressChanged += wc_DownloadProgressChanged;
+
+                    toolStripStatusLabel1.Text = (comboBox1.SelectedItem as dynamic).Text + " Downloading. Please Wait..";
+                    wc.DownloadFileAsync(
+                        // Param1 = Link of file
+                        new System.Uri(link), dpath += "/" + fname
+                    // Param2 = Path to save
+                    );
+
+                    wc.DownloadFileCompleted += Wc_DownloadFileCompleted;
+                }
+            }
+            catch { }
+        }
+
+        private void Wc_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            try
+            {
+                toolStripStatusLabel1.Text = (comboBox1.SelectedItem as dynamic).Text + " Downloaded";
+                button1.Enabled = true;
+                Process.Start(dpath);
+                showprogressbar(false);
+            }
+            catch { }
+        }
+
+        private string writeresource(string tempExeName, byte[] resource)
+        {
+            
+            try
+            {
+                if (!Directory.Exists(dpath))
+                {
+                    Directory.CreateDirectory(dpath);
+                }
+                if (File.Exists(tempExeName))
+                {
+                    File.Delete(tempExeName);
+                }
+                using (FileStream fsDst = new FileStream(tempExeName, FileMode.CreateNew, FileAccess.Write))
+                {
+                    byte[] bytes = resource;
+                    fsDst.Write(bytes, 0, bytes.Length);
+                }
+                return tempExeName;
+            }
+            catch
+            {
+                return tempExeName;
+            }
+        }
+
+        private void microsoftOfficeActivationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string tempExeName = Path.Combine(dpath, "MAS_AIO.cmd");
+                Process.Start(writeresource(tempExeName, Resources.MAS_AIO), "/Ohook");
+            }
+            catch { }
+        }
+
+        private void mincrosoftWindowsActivationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            dpath = Application.StartupPath + "/apps";
+            try
+            {
+                string tempExeName = Path.Combine(dpath, "MAS_AIO.cmd");
+                Process.Start(writeresource(tempExeName, Resources.MAS_AIO), "/HWID");
+            }
+            catch { }
+        }
+
+        private void updateRepositoriesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            update_repos();
+            //backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+            //toolStripStatusLabel1.Text = "Repos updated.";
+            
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+           // update_repos();
+            //File.Delete(dpath);
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DeleteReadOnlyDirectory(dpath);
+           // Directory.Delete(dpath, true);
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            about_form about_Form = new about_form();
+            about_Form.ShowDialog();
+        }
+    }
+}
